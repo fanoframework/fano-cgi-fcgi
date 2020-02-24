72a0800276717e901dd0de54649ee6468dafe58a
zamroni <zamroni@dominopos.com>
2020-02-24 10:25:03 +0800

    "Initial commit"

---
+++ src/app.pas
@@ -0,0 +1,35 @@
+(*!------------------------------------------------------------
+ * [[APP_NAME]] ([[APP_URL]])
+ *
+ * @link      [[APP_REPOSITORY_URL]]
+ * @copyright Copyright (c) [[COPYRIGHT_YEAR]] [[COPYRIGHT_HOLDER]]
+ * @license   [[LICENSE_URL]] ([[LICENSE]])
+ *------------------------------------------------------------- *)
+program app;
+
+uses
+
+    fano,
+    bootstrap;
+
+var
+    appInstance : IWebApplication;
+
+begin
+
+    (*!-----------------------------------------------
+     * Bootstrap Fast CGI application
+     *
+     * @author AUTHOR_NAME <author@email.tld>
+     *------------------------------------------------*)
+    appInstance := TDaemonWebApplication.create(
+        TFastCgiAppServiceProvider.create(
+            TServerAppServiceProvider.create(
+                TAppServiceProvider.create(),
+                (TBoundSvrFactory.create(stdInputHandle) as ISocketSvrFactory).build()
+            )
+        ),
+        TAppRoutes.create()
+    );
+    appInstance.run();
+end.
